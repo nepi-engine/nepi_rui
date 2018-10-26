5b6e96af58556ca9dd75ee5e494e713be774ddeb
Lucas Doyle <lucas.p.doyle@gmail.com>
2018-10-25 20:20:02 -0700

    ROS connection logic and logging

--- rui_webserver/rui-app/src/App.js
+++ rui_webserver/rui-app/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react"
 import Toggle from "react-toggle"
 
+import ROS from "roslib"
+
 import Page from "./Page"
 import Nav from "./Nav"
 import Section from "./Section"
@@ -8,19 +10,90 @@ import HorizontalDivider from "./HorizontalDivider"
 import { Columns, Column } from "./Columns"
 import Label from "./Label"
 
+const ros = new ROS.Ros({
+  url: "ws://localhost:9090"
+})
+
+// rostopic pub /listener std_msgs/String "Hello, World"
+const rosListener = new ROS.Topic({
+  ros,
+  name: "/listener",
+  messageType: "std_msgs/String"
+})
+
+const ROS_CONNECTION_STATE_CONNECTED = "CONNECTED"
+const ROS_CONNECTION_STATE_DISCONNECTED = "DISCONNECTED"
+const ROS_CONNECTION_STATE_ERROR = "ERROR"
+
 class App extends Component {
   constructor(props) {
     super(props)
 
     this.state = {
+      rosConnectionState: ROS_CONNECTION_STATE_DISCONNECTED,
+      rosConnectionError: null,
+      rosLog: "",
       text: "test1",
       disabledText: "test2",
       toggle: true,
       disabledToggle: false
     }
 
+    this.onConnectedToROS = this.onConnectedToROS.bind(this)
+    this.onConnectedToROSError = this.onConnectedToROSError.bind(this)
+    this.onDisconnectedToROS = this.onDisconnectedToROS.bind(this)
+
+    this.rosLog = this.rosLog.bind(this)
+    this.onROSListener = this.onROSListener.bind(this)
+
     this.onInputChange = this.onInputChange.bind(this)
     this.onToggleChange = this.onToggleChange.bind(this)
+  }
+
+  componentDidMount() {
+    ros.on("connection", this.onConnectedToROS)
+    ros.on("error", this.onConnectedToROSError)
+    ros.on("close", this.onDisconnectedToROS)
+
+    rosListener.subscribe(this.onROSListener)
+  }
+
+  componentWillUnmount() {
+    ros.off("connection", this.onConnectedToROS)
+    ros.off("error", this.onConnectedToROSError)
+    ros.off("close", this.onDisconnectedToROS)
+
+    rosListener.unsubscribe()
+  }
+
+  onConnectedToROS() {
+    this.setState({
+      rosConnectionState: ROS_CONNECTION_STATE_CONNECTED,
+      rosConnectionError: null
+    })
+    this.rosLog("Connected to rosbridge websocket server.")
+  }
+
+  onConnectedToROSError(error) {
+    this.setState({
+      rosConnectionState: ROS_CONNECTION_STATE_ERROR,
+      rosConnectionError: error
+    })
+    this.rosLog("Error connecting to rosbridge websocket server: ", error)
+  }
+
+  onDisconnectedToROS() {
+    this.setState({ rosConnectionState: ROS_CONNECTION_STATE_DISCONNECTED })
+    this.rosLog("Connection to rosbridge websocket server closed.")
+  }
+
+  rosLog(text) {
+    const { rosLog } = this.state
+    this.setState({ rosLog: rosLog + `${text}\n` })
+  }
+
+  onROSListener(message) {
+    this.rosLog(`Received message on ${rosListener.name}: ${message.data}`)
   }
 
   onInputChange(e) {
@@ -32,7 +105,15 @@ class App extends Component {
   }
 
   render() {
-    const { text, disabledText, toggle, disabledToggle } = this.state
+    const {
+      rosConnectionState,
+      rosConnectionError,
+      rosLog,
+      text,
+      disabledText,
+      toggle,
+      disabledToggle
+    } = this.state
     return (
       <Page>
         <Nav />
@@ -52,6 +133,13 @@ class App extends Component {
               <Label title={"Unmodifiable toggle"}>
                 <Toggle disabled checked={disabledToggle} />
               </Label>
+            </Section>
+            <Section title={"ROS status"}>
+              <Label title={"ROS Connection"}>
+                <input disabled value={rosConnectionState} />
+              </Label>
+              {rosConnectionError && `Connection error: ${rosConnectionError}`}
+              <pre>{rosLog}</pre>
             </Section>
           </Column>
           <Column>
