72c8c28a72f9f2b67719aa4dc200451b2a80b059
Mike Lautman <Mike@PickNik.ai>
2018-11-07 13:16:30 -0700

    Fixes from a run through of the setup and run commands

--- devenv.sh
+++ devenv.sh
@@ -2,9 +2,9 @@
 # this is my standard environment setup
 
 # use the correct python and npm
-source venv/bin/activate || echo 'Failed to load venv'
-nvm use
+source $CATKIN_WS/venv/bin/activate || echo "Failed to load venv"
 
 # set environment variables
 export RUI_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && nvm use )"
 
