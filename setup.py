011e4d375a0ef0934ced75d2c3ced2edf75649a0
Mike Lautman <mikeblautman@gmail.com>
2018-11-14 13:10:45 -0700

    Python pkg (#7)
    * example python publisher
    
    * making a more general python ros library

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['numurus_rui'],
-    scripts=['scripts/nd_status_pub.py']
+    package_dir={'': 'src'}
 )
 
 setup(**d)
