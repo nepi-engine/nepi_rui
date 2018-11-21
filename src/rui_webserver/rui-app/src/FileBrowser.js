2f9388e5b072fdf49ce4ef6c692af33ba523d970
Lucas Doyle <lucas.p.doyle@gmail.com>
2018-11-21 17:17:51 -0500

    misc

--- src/rui_webserver/rui-app/src/FileBrowser.js
+++ src/rui_webserver/rui-app/src/FileBrowser.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react"
-import { Route, Switch, Link } from "react-router-dom"
+import { Link } from "react-router-dom"
 import filesize from "filesize"
 
 const FLASK_URL = "http://localhost:5003"
