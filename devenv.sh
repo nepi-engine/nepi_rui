f2e21770a98fe6d8d6a91de41f4a49d96c5b5d36
Josh Maximoff <jmaximoff@numurus.com>
2019-08-28 12:37:58 -0400

    Fix a couple of path/dependency issues with the build

--- devenv.sh
+++ devenv.sh
@@ -2,7 +2,7 @@
 # Setup environment
 
 # Use the correct python and npm
-source ../../venv/bin/activate || echo "Failed to load venv"
+source ./venv/bin/activate || echo "Failed to load venv"
 
 # Set environment variables
 export RUI_HOME="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
