17f46a34727112568adb9cdd435096acf6073309
Josh Maximoff <jmaximoff@numurus.com>
2020-05-20 16:23:54 -0400

    Add a proper launch file (including rosbridge_websocket)

--- etc/start_rui.sh
+++ etc/start_rui.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-
+sleep 5 #Hack to allow other components to start first
 source /opt/numurus/ros/setup.bash
 
 export RUI_HOME="/opt/numurus/ros/share/numurus_rui"
@@ -10,4 +10,5 @@ source ${RUI_HOME}/venv/bin/activate || echo "Failed to load venv"
 # Set environment variables
 export PYTHONPATH="$PYTHONPATH:$RUI_HOME/src"
 
-rosrun numurus_rui run_webserver.py
+roslaunch numurus_rui numurus_rui.launch
+#rosrun numurus_rui run_webserver.py
