81344eeb57bba42584a970a3986df400ca7889ec
Josh Maximoff <jmaximoff@numurus.com>
2020-06-23 12:38:49 -0400

    Start the RUI ROS components in the device namespace, not global namespace
    HMI reported a problem with their own rosbridge due to conflict with ours, both of which ran at the global namespace. This commit does what should have been done originally; start ALL device components in the device namespace.

--- etc/start_rui.sh
+++ etc/start_rui.sh
@@ -2,6 +2,24 @@
 sleep 5 #Hack to allow other components to start first
 source /opt/numurus/ros/setup.bash
 
+# The sys_env script must exist and be valid. The committed base file is
+# (intentionally) not valid because TBD fields are not populated
+if [ ! -f /opt/numurus/ros/etc/sys_env.bash ]; then
+	echo "ERROR! Could not find /opt/numurus/ros/etc/sys_env.bash"
+	exit 1
+fi
+
+source /opt/numurus/ros/etc/sys_env.bash
+if [ "$DEVICE_TYPE" = "TBD" ]; then
+	echo "ERROR! DEVICE_TYPE must be set in /opt/numurus/ros/sys_env.bash"
+	exit 1
+fi
+
+if [ "$DEVICE_SN" = "TBD" ]; then
+	echo "ERROR! DEVICE_SN must be set in /opt/numurus/ros/sys_env.bash"
+	exit 1
+fi
+
 export RUI_HOME="/opt/numurus/ros/share/numurus_rui"
 
 # Use the correct python
