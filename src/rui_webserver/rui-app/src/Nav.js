c33aa5adbf58e85c4cd9682236a77bc48b3f501c
Josh Maximoff <jmaximoff@numurus.com>
2020-11-02 15:30:01 -0500

    Enlarge logo container to make text legible

--- src/rui_webserver/rui-app/src/Nav.js
+++ src/rui_webserver/rui-app/src/Nav.js
@@ -9,7 +9,7 @@ import logo from "./logo.png"
 const styles = Styles.Create({
   logo: {
     // backgroundColor: Styles.vars.colors.grey0
-    width: "100px",
+    width: "150px",
     //height: "40px"
   },
   navItem: {
@@ -130,7 +130,7 @@ class Nav extends Component {
         <Columns>
           <Column>
             <div style={styles.logo}>
-              <img src={logo} alt={"logo"} max-width={"30%"} height={"auto"}/>
+              <img src={logo} alt={"logo"} max-width={"50%"} height={"auto"}/>
             </div>
           </Column>
           {!pageLocked && (
