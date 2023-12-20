724daba9c229c86d37095d2814fcc0ffbe86c476
Josh Maximoff <jmaximoff@numurus.com>
2023-12-20 10:01:39 -0500

    Update rsync script

--- rsync_workspace_to_target.sh
+++ rsync_workspace_to_target.sh
@@ -24,6 +24,20 @@
 ##
 ##
 
+#######################################################################################################
+# Usage: $ ./rsync_workspace_to_target
+#
+# This script is intended for the scenario where NEPI source code is cloned (and modified as applicable
+# on a development host and then pushed to the NEPI target device to be built natively on that device.
+#
+# If you are cloning NEPI source directly to the target device, you do not need to run this script.
+#
+# The script leverages some environment variables if they exist to allow user customization:
+#    NEPI_SSH_KEY: Path to SSH private keyfile for the NEPI device
+#    NEPI_TARGET_IP: IP address (or resolvable hostname) for the NEPI device
+# In the absence of these environment variables the script attempts to use hard-coded defaults.
+#######################################################################################################
+
 if [[ -z "${NEPI_SSH_KEY}" ]]; then
   echo "No NEPI_SSH_KEY environment variable... will use the default key path"
   NEPI_SSH_KEY="~/.ssh/numurus/nepi_default_ssh_key_ed25519"
@@ -37,5 +51,12 @@ fi
 # Also generate the top-level version
 git describe --dirty > ./etc/nepi_rui_fw_version.txt
 
-# And copy RUI front-end source code to the /opt/numurus/ros/share folder, since that is where it is built
-rsync -avzhe "ssh -i ${NEPI_SSH_KEY}" --exclude .git* --exclude build_* --exclude devel_* --exclude logs_* --exclude install_* ../nepi_rui/ nepi@${NEPI_TARGET_IP}:/opt/nepi/nepi_rui
+# The target source directory is hard-coded here as the source must be built and installed right in-place at an expected location
+NEPI_RUI_TARGET_SRC_DIR="/opt/nepi/nepi_rui"
+
+# Avoid pushing local build artifacts, git stuff, and any local venv or node.js modules that are locally installed
+RSYNC_EXCLUDES="--exclude rsync_workspace_to_target.sh --exclude .git* \
+--exclude venv --exclude src/rui_webserver/rui-app/node_modules"
+
+# Push (almost) everything to the specified source folder on the target
+rsync -avzhe "ssh -i ${NEPI_SSH_KEY} -o StrictHostKeyChecking=no" ${RSYNC_EXCLUDES} ../nepi_rui/ nepi@${NEPI_TARGET_IP}:${NEPI_RUI_TARGET_SRC_DIR}
